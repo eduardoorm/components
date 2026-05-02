d99883b8b09ab131d7bde363ed97161a0062229d
José Eduardo Ormeño Meneses <74284944+eduardoorm@users.noreply.github.com>
2026-05-02 15:19:40 -0500

    nuevo cambio

--- src/dev-mode-types.d.ts
+++ src/dev-mode-types.d.ts
@@ -1,2 +1,3 @@
 /** Variable replaced at build time that indicates whether the app is in development mode. */
+/** Variable replaced at build time that indicates whether the app is in development mode. */
 declare const ngDevMode: object | null;
